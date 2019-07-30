02adab6f234504f8a176e7645209720f42869b49
Yuki-Suetake <yuki.suetake@outlook.jp>
2019-07-30 05:55:43 +0000

    microposts3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
     <div class='center jumbotron'>
         <div class='text=center'>
             <h1>welcome to the Microposts</h1>
             {!! link_to_route('signup.get', 'sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
+    @elseif
 @endsection
     
